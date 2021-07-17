325fbc8226262f2a7f46b3875db84ff927aec87e
叶建锋 <1061022701@qq.com>
2021-07-17 18:53:28 +0900

    add another long method to test branch

--- src/com/test/Demo1.java
+++ src/com/test/Demo1.java
@@ -11,4 +11,8 @@ public class Demo1 {
 			String param7, String param8, String param9, String param10, String param11) {
 		return null;
 	}
+	public List<String> testBranchLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongLongName(String param1, String param2, String param3, String param4, String param5, String param6,
+			String param7, String param8, String param9, String param10, String param11) {
+		return null;
+	}	
 }
