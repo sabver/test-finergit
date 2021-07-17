b95c6945272aa8ebca12fc1b3ce48d1bab4c82a9
叶建锋 <1061022701@qq.com>
2021-07-17 18:45:39 +0900

    first commit

---
+++ src/com/test/Demo1.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import java.util.List;
+
+public class Demo1 {
+	public List<String> masterLongLongLongLongLongLongLongLongName(String param1, String param2, String param3, String param4, String param5, String param6,
+			String param7, String param8, String param9, String param10, String param11) {
+		return null;
+	}
+}
